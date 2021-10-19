c40a1cac07d6cb15b644bd23ee5f1a3ab8d08bdb
Ivan Rojo <ivanrojo07@gmail.com>
2021-10-19 11:36:39 -0500

    Crear items del personaje y dibujarlos

--- src/views/Hero/Index.vue
+++ src/views/Hero/Index.vue
@@ -8,6 +8,7 @@
             <!-- En 'lg' orden 2 -->
             <b-col md="12" lg="8" order-lg="2">
                 <BaseLoading v-if="isLoadingItems"/>
+                <HeroItems v-if="items" :items="items" />
             </b-col>
 
             <!-- 12 columnas de 'xs' -> 'md', 4 columnas desde 'lg' hacia arriba -->
@@ -27,7 +28,7 @@ import setError from '@/mixins/setError'
 import BaseLoading from '@/components/BaseLoading'
 import HeroDetailHeader from './HeroDetailHeader'
 import HeroAttributes from './HeroAttributes/Index'
-// import HeroItems from './HeroItems/Index'
+import HeroItems from './HeroItems/Index'
 import HeroSkills from './HeroSkills/Index'
 import { getApiHero, getApiDetailedHeroItems } from '@/api/search'
 export default {
@@ -37,7 +38,8 @@ export default {
         BaseLoading,
         HeroDetailHeader,
         HeroAttributes,
-        HeroSkills
+        HeroSkills,
+        HeroItems
     },
     data () {
         return {
